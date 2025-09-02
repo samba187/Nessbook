c21e76de9ceecd6a5cb07410b2a61f17bf9243d3
samba sorbo <sorbosamba93800@gmail.com>
2025-09-02 11:00:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -310,4 +310,5 @@ def get_genres():
     return jsonify(genres)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", "5000"))
+    app.run(host="0.0.0.0", port=port, debug=True)
